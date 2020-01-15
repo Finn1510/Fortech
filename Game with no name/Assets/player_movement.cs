39d109997dc9663e0cef907fc443ea2f5752e639
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-01-15 17:47:01 +0100

    added demo character controller

---
+++ Game with no name/Assets/player_movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float Horizontalmove = 0f;
+    public float runSpeed = 40f;
+    bool Jump = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontalmove = Input.GetAxisRaw("Horizontal") * runSpeed; 
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(Horizontalmove * Time.fixedDeltaTime , false, Jump);
+        Jump = false;
+    }
+
+}
