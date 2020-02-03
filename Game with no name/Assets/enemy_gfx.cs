5f1473300598970e492909e20742f57d54a5de71
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-02-03 17:07:08 +0100

    enemy spawn system

---
+++ Game with no name/Assets/enemy_gfx.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class enemy_gfx : MonoBehaviour
+{
+    public AIPath noicepath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(noicepath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } 
+        else if(noicepath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
