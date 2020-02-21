26f22719ea1e6dd3d197b5d1f85804f674075c0f
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-02-21 10:49:31 +0100

    weapon networking removed

--- Game with no name/Assets/enemy_gfx.cs
+++ Game with no name/Assets/enemy_gfx.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding;
+using Pathfinding; 
+
 
 public class enemy_gfx : MonoBehaviour
 {
     public AIPath noicepath;
+    
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if(noicepath.desiredVelocity.x >= 0.01f)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
-        } 
+        }
+
         else if(noicepath.desiredVelocity.x <= -0.01f)
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
