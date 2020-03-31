0d92258908dd091bf1a66b98a70bdea4b1504683
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-03-31 16:35:15 +0200

    added DOTween

--- Game with no name/Assets/PlayerHandler.cs
+++ Game with no name/Assets/PlayerHandler.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 
 public class PlayerHandler : MonoBehaviour
 {
+    [Header("Parameters")]
     [SerializeField] float PlayerHealth = 100f;
     [SerializeField] Slider HealthBar;
-    
-    
+
+    PostProcessVolume volume;
+    Vignette vignette;
+
     // Start is called before the first frame update
     void Start()
     {
-        HealthBar.maxValue = PlayerHealth;   
+        HealthBar.maxValue = PlayerHealth;
+        PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, vignette);
+
     }
 
     // Update is called once per frame
