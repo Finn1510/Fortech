05d9ad7699ebf5a2c93619497ab31e645659d4c1
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-03-31 16:58:50 +0200

    experimental effects

--- Game with no name/Assets/PlayerHandler.cs
+++ Game with no name/Assets/PlayerHandler.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Rendering.PostProcessing;
+using DG.Tweening;
 
 public class PlayerHandler : MonoBehaviour
 {
     [Header("Parameters")]
     [SerializeField] float PlayerHealth = 100f;
     [SerializeField] Slider HealthBar;
+
+    float healthTemp;
 
     PostProcessVolume volume;
     Vignette vignette;
@@ -18,17 +21,34 @@ public class PlayerHandler : MonoBehaviour
     {
         HealthBar.maxValue = PlayerHealth;
         PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, vignette);
+        vignette.intensity.value = 0f;
+        vignette.color.value = Color.red;
+        vignette.enabled.Override(true);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        HealthBar.value = PlayerHealth;    
+        HealthBar.value = PlayerHealth;
+        DamageEffects();
     } 
 
     public void Damage(float Damage)
     {
         PlayerHealth = PlayerHealth - Damage;
+        
+    } 
+
+    void DamageEffects()
+    {
+        if(PlayerHealth < healthTemp)
+        {
+            healthTemp = PlayerHealth;
+            if (vignette.intensity.value < 1f)
+            {
+                DOTween.To(() => vignette.intensity.value, x => vignette.intensity.value = x, 0.5f, 1f);
+            }
+        }
     }
 }
