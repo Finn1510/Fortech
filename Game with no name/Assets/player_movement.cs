846e1250235eb93c9676da500b04c65aae26300b
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-07-29 18:33:03 +0200

    added HealthSystem
    no Death yet tho

--- Game with no name/Assets/player_movement.cs
+++ Game with no name/Assets/player_movement.cs
@@ -1,13 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
 
 public class player_movement : MonoBehaviour
 {
-    public CharacterController2D controller;
+    [Header("Parameters")]
+    [SerializeField] float runSpeed = 40f;
+    [SerializeField] float Health = 100;
     float Horizontalmove = 0f;
-    public float runSpeed = 40f;
+
+    [Space]
+
+    [Header("References")]
+    [SerializeField] CharacterController2D controller;
+    [SerializeField] Text Healthtext;
+    [SerializeField] Slider HealthSlider;
+    CinemachineImpulseSource ImpulseGEN;
+
     bool Jump = false;
+
+    void Start()
+    {
+        Healthtext.text = Health.ToString();
+        HealthSlider.maxValue = Health;
+        HealthSlider.value = Health;
+
+        ImpulseGEN = GetComponent<CinemachineImpulseSource>();
+    }
     
     // Update is called once per frame
     void Update()
@@ -23,6 +45,14 @@ public class player_movement : MonoBehaviour
     {
         controller.Move(Horizontalmove * Time.fixedDeltaTime , false, Jump);
         Jump = false;
+    } 
+
+    public void Damage (float amount)
+    {
+        Health = Health - amount;
+        Healthtext.text = Health.ToString();
+        HealthSlider.value = Health;
+        ImpulseGEN.GenerateImpulse(new Vector3(2, 2, 0));
     }
 
 }
