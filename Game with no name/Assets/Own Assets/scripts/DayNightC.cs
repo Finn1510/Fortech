3ab591f88630a66f0e829d8e81f74215082c87f7
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-08-22 21:02:05 +0200

    Day/Night Cycle with save system

--- Game with no name/Assets/Own Assets/scripts/DayNightC.cs
+++ Game with no name/Assets/Own Assets/scripts/DayNightC.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.LWRP;
 using DG.Tweening;
+using UnityEngine.Rendering;
+using System.Reflection;
+using UnityEditorInternal;
+using System;
 
 public class DayNightC : MonoBehaviour
 {
@@ -10,30 +14,159 @@ public class DayNightC : MonoBehaviour
     [SerializeField] Light2D SunLight;
 
     [Header("Parameters")]
-    [Range(1f, 30f)]
-    [SerializeField] float DayDurationMinutes = 10f;
-    [Range(1f, 30f)]
-    [SerializeField] float NightDurationMinutes = 5f;
+    [Range(0.1f, 10f)]
+    [SerializeField] float stateDurationMinutes = 1f;
+    
 
     [SerializeField] float DayLightIntensity = 1;
     [SerializeField] float NightLightIntensity = 0.2f;
+    [SerializeField] string WorldStartState = "State3";
 
-    float RealDayDuration;
-    float RealNightDuration;
+
+    float RealStateDuration;
+    int StateIndex;
+    string methodName;
 
     private void Start()
     {
-        RealDayDuration = DayDurationMinutes * 60;
-        RealNightDuration = NightDurationMinutes * 60;
+        RealStateDuration = stateDurationMinutes * 60;
+
+        if(ES3.KeyExists("currentStateIndex") == true)
+        {
+            StateIndex = ES3.Load<int>("currentStateIndex");
+            if (StateIndex != 10)
+            {
+                methodName = "State" + StateIndex;
+            }
+            else
+            {
+                methodName = "State1";
+            }
+
+            
+        }
+
+        else
+        {
+            methodName = WorldStartState;
+        }
+
+        Type thisType = this.GetType();
+        MethodInfo theMethod = thisType.GetMethod(methodName);
+        theMethod.Invoke(this, null);
+
     }
 
-    void Day()
+
+    public void State1()
     {
-        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, DayLightIntensity, RealDayDuration);
+        Debug.Log("State1");
+        StateIndex = 1;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 0.36f, RealStateDuration);
+        StartCoroutine(StateWait());
     }
 
-    void Night()
+    public void State2()
     {
-        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, NightLightIntensity, RealNightDuration);
+        Debug.Log("State2");
+        StateIndex = 2;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 0.52f, RealStateDuration);
+        StartCoroutine(StateWait());
     }
+
+    public void State3()
+    {
+        Debug.Log("State3");
+        StateIndex = 3;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 0.68f, RealStateDuration);
+        StartCoroutine(StateWait());
+    }
+
+    public void State4()
+    {
+        Debug.Log("State4");
+        StateIndex = 4;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 0.84f, RealStateDuration);
+        StartCoroutine(StateWait());
+    }
+
+    public void State5()
+    {
+        Debug.Log("State5");
+        StateIndex = 5;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 1, RealStateDuration);
+        StartCoroutine(StateWait());
+    }
+
+    public void State6()
+    {
+        Debug.Log("State6");
+        StateIndex = 6;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 0.84f, RealStateDuration);
+        StartCoroutine(StateWait());
+    }
+
+    public void State7()
+    {
+        Debug.Log("State7");
+        StateIndex = 7;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 0.68f, RealStateDuration);
+        StartCoroutine(StateWait());
+    }
+
+    public void State8()
+    {
+        Debug.Log("State8");
+        StateIndex = 8;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 0.52f, RealStateDuration);
+        StartCoroutine(StateWait());
+    }
+
+    public void State9()
+    {
+        Debug.Log("State9");
+        StateIndex = 9;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 0.36f, RealStateDuration);
+        StartCoroutine(StateWait());
+    }
+
+
+    public void State10()
+    {
+        Debug.Log("State10");
+        StateIndex = 10;
+        DOTween.To(() => SunLight.intensity, x => SunLight.intensity = x, 0.2f, RealStateDuration);
+        StartCoroutine(StateWait());
+    }
+    
+    
+    IEnumerator StateWait()
+    {
+        yield return new WaitForSeconds(RealStateDuration);
+        
+        if(StateIndex != 10)
+        {
+            methodName = "State" + (StateIndex + 1 );
+        } 
+        else
+        {
+            methodName = "State1";
+        }
+
+        Type thisType = this.GetType();
+        MethodInfo theMethod = thisType.GetMethod(methodName);
+        theMethod.Invoke(this, null);
+    }  
+
+    void Save()
+    {
+        ES3.Save<int>("currentStateIndex", StateIndex);
+    }
+
+    private void OnApplicationQuit()
+    {
+        Save();
+    }
+
+
 }
