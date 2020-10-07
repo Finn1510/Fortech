5a26735d0bb1004b9650aabb45372a970a088632
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-10-07 20:19:30 +0200

    fixed weired CrazyEye flipping

--- Game with no name/Assets/Own Assets/scripts/CrazyEyeAi.cs
+++ Game with no name/Assets/Own Assets/scripts/CrazyEyeAi.cs
@@ -96,18 +96,24 @@ public class CrazyEyeAi : MonoBehaviour
         Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
         Vector2 force = direction * speed * Time.deltaTime;
 
-        if (force.x >= 0.01f)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
-        else if (force.x <= -0.01f)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
+        
 
         rb.AddForce(force);
         
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]); 
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        //only change flip when waypoint not reached to prevent looking back to passed waypoint
+        if (distance > nextWaypointDistance)
+        {
+            if (force.x >= 0.01f)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (force.x <= -0.01f)
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
 
         if(distance < nextWaypointDistance)
         {
