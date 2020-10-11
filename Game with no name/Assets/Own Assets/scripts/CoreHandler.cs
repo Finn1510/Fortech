ceb98cd7573a452d14a114f0c307ff0378803784
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-10-11 22:38:11 +0200

    extended Corehadler

--- Game with no name/Assets/Own Assets/scripts/CoreHandler.cs
+++ Game with no name/Assets/Own Assets/scripts/CoreHandler.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CoreHandler : MonoBehaviour
 {
+    [Header("References")]
+    CinemachineImpulseSource ImpulseGEN;
+    AudioSource damageAudio;
+    [Header("Parameters")]
+    [SerializeField] float maxHealth = 500;
+
+    public float Health;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        damageAudio = GetComponent<AudioSource>();
+        ImpulseGEN = GetComponent<CinemachineImpulseSource>();
     }
 
     // Update is called once per frame
@@ -15,4 +25,15 @@ public class CoreHandler : MonoBehaviour
     {
         
     }
+
+    public void Damage(float amount)
+    {
+        Health = Health - amount;
+        
+        damageAudio.Play();
+        ImpulseGEN.GenerateImpulse(new Vector3(2, 2, 0));
+        
+
+    }
+
 }
