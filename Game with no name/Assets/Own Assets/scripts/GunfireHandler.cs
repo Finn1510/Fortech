41c252a8255c0f92d84a6ca775251a9009d3c309
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-10-12 23:16:12 +0200

    fixed shot bullet vfx properly deleting itself

---
+++ Game with no name/Assets/Own Assets/scripts/GunfireHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunfireHandler : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] float DestroyDelay = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyDelayCorut());    
+    }
+
+    IEnumerator DestroyDelayCorut()
+    {
+        yield return new WaitForSeconds(DestroyDelay);
+        Destroy(gameObject);
+    }
+}
