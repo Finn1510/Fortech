6e7a24908e204bf07044f4610c305b9cdd452d3d
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-11-05 12:47:05 +0100

    [inf] connection with database+MySQL package chosen+began devellopment of debug tool
    -finally MySQL package chosen
    -first connection with database
    -began devellopment of the FortechAdminUtillityTool (C# windows forms) for better debugging

---
+++ FortechAdminUtilityTool/FortechAdminUtilityTool/FortechAdminUtilityTool/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace FortechAdminUtilityTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsernameText = new System.Windows.Forms.Label();
+            this.SelectUserbutton = new System.Windows.Forms.Button();
+            this.PasswordText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UsernameText
+            // 
+            this.UsernameText.AutoSize = true;
+            this.UsernameText.Font = new System.Drawing.Font("Open Sans Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameText.Location = new System.Drawing.Point(12, 9);
+            this.UsernameText.Name = "UsernameText";
+            this.UsernameText.Size = new System.Drawing.Size(157, 38);
+            this.UsernameText.TabIndex = 0;
+            this.UsernameText.Text = "Username";
+            // 
+            // SelectUserbutton
+            // 
+            this.SelectUserbutton.Font = new System.Drawing.Font("Open Sans", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SelectUserbutton.Location = new System.Drawing.Point(13, 392);
+            this.SelectUserbutton.Name = "SelectUserbutton";
+            this.SelectUserbutton.Size = new System.Drawing.Size(106, 38);
+            this.SelectUserbutton.TabIndex = 1;
+            this.SelectUserbutton.Text = "select  User";
+            this.SelectUserbutton.UseVisualStyleBackColor = true;
+            this.SelectUserbutton.Click += new System.EventHandler(this.SelectUserbutton_Click);
+            // 
+            // PasswordText
+            // 
+            this.PasswordText.AutoSize = true;
+            this.PasswordText.Font = new System.Drawing.Font("Open Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordText.Location = new System.Drawing.Point(15, 47);
+            this.PasswordText.Name = "PasswordText";
+            this.PasswordText.Size = new System.Drawing.Size(85, 23);
+            this.PasswordText.TabIndex = 2;
+            this.PasswordText.Text = "password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PasswordText);
+            this.Controls.Add(this.SelectUserbutton);
+            this.Controls.Add(this.UsernameText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UsernameText;
+        private System.Windows.Forms.Button SelectUserbutton;
+        private System.Windows.Forms.Label PasswordText;
+    }
+}
+
---
+++ Game with no name/Assets/Own Assets/scripts/databaseSync.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data;
+using MySql.Data.MySqlClient; 
+
+
+public class databaseSync : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //connection parameters
+        string connparams = "server=johnny.heliohost.org;user=finn15_FortechUser;database=finn15_InformatikProjekt;port=3306;password=averystrongpassword";
+
+        MySqlConnection conn = new MySqlConnection(connparams);
+        
+        //try to connect to database 
+        try
+        {
+            
+            conn.Open();
+            Debug.Log("Connected to database");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+
+    } 
+
+    void CreateAccount(string Username, string Password)
+    {
+
+    } 
+
+    void Login(string Username, string Password)
+    {
+
+    }
+}
