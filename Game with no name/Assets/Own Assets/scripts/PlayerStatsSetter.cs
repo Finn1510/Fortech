900ee5c8c9cfb4b6c731d8de0f24197b81f8be2e
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-12-08 09:04:30 +0100

    PlayerStats UI

---
+++ Game with no name/Assets/Own Assets/scripts/PlayerStatsSetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerStatsSetter : MonoBehaviour
+{
+    [SerializeField] TMP_Text CrazyEyesKilledText;
+    [SerializeField] TMP_Text ZombiesKilledText;
+    [SerializeField] TMP_Text DeathsText;
+    [SerializeField] TMP_Text DamageDealt;
+    [SerializeField] TMP_Text ShotsFired;
+
+    PlayerStats PlayerStatsData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerStatsData = GetComponent<PlayerStats>();
+
+        CrazyEyesKilledText.text = "CrazyEyes: " + PlayerStatsData.CrazyEyesKilled.ToString();
+        ZombiesKilledText.text = "Zombies: " + PlayerStatsData.ZombiesKilled.ToString();
+        DeathsText.text = "Deaths: " + PlayerStatsData.DiedCount.ToString();
+        DamageDealt.text = "Damage dealt: " + PlayerStatsData.DamageDealed.ToString();
+        ShotsFired.text = "Shots fired: " + PlayerStatsData.ShotsFired.ToString();
+    }
+
+    
+    
+}
