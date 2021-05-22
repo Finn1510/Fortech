a8445fba00dfcfc447a8aa56e0c03f16ee252562
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2021-05-22 15:13:11 +0200

    cleaned up a bit - removed some unused stuff

---
+++ Game with no name/Assets/Own Assets/scripts/InventoryHack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventoryHack : MonoBehaviour
+{
+    [SerializeField] Button AddToInventoryButton;
+    [SerializeField] TMP_Dropdown ItemTypeDropDown;
+    [SerializeField] TMP_InputField AmountInputfield;
+
+    private void Start()
+    {
+        
+    }
+
+    public void AddToInventory()
+    {
+
+    }
+}
